6980aec3836ff71b7dc78e5de35f28963c668078
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-09-22 02:25:04 +0000

    Added day counter and join date JSON in standard format

--- edutale/src/Mainpage/Day/Day.jsx
+++ edutale/src/Mainpage/Day/Day.jsx
@@ -1,7 +1,15 @@
+import * as USER from "../../USER.json"
+
 export default function Day() {
     return (
       <div className="day">
-        <h1><u> Day 1 </u></h1>
+        <h1><u> Day {dayNum()} </u></h1>
       </div>
     )
   }
+
+const dayNum = function() {
+    let now = Date.now()
+    let joinDate = Date.parse(USER.joinDate)
+    return Math.round((now - joinDate) / 86400000)
+}
