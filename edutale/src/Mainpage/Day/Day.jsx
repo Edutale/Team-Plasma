891b7b08d1fda4522d93c571f9c8a5041b88e298
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-09-22 02:43:39 +0000

    Added display for exp amount and level of student

--- edutale/src/Mainpage/Day/Day.jsx
+++ edutale/src/Mainpage/Day/Day.jsx
@@ -3,13 +3,16 @@ import * as USER from "../../USER.json"
 export default function Day() {
     return (
       <div className="day">
-        <h1><u> Day {dayNum()} </u></h1>
+        <h1><u> Day {dayNum(USER.joinDate)} </u></h1>
       </div>
     )
-  }
+}
 
-const dayNum = function() {
+// calculates day number since the join date of the user.
+// done by subtracting the number of milliseconds between
+// now and the join date, then dividing by the num of ms/day.
+function dayNum(joinDate) {
     let now = Date.now()
-    let joinDate = Date.parse(USER.joinDate)
-    return Math.round((now - joinDate) / 86400000)
+    let joinDateParsed = Date.parse(joinDate)
+    return Math.round((now - joinDateParsed) / 86400000)
 }
