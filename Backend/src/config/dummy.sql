bf37b1f54b157b4e6d5137e2b96d4b32224833ec
LingCPU <115831204+LingCPU@users.noreply.github.com>
2024-09-23 20:38:34 -0700

    Add files via upload
    dummy data for database

---
+++ Backend/src/config/dummy.sql
@@ -0,0 +1,108 @@
+insert into Student values
+('111111111', 'Human', 'some@email.com'),
+('000000000', 'Person', 'not@an.email'),
+('999999999', 'Name', 'user@domain.com'),
+('123456789', 'Student', 'student@school.edu'),
+('987654321', 'TeachersPet', 'teacherspet@school.edu');
+
+insert into Major values
+('0000', 'Undecided'),
+('0001', 'Computer Science'),
+('0002', 'Computer Engineering');
+
+insert into Student_Major values
+('111111111', '0000'),
+('000000000', '0000'),
+('999999999', '0001'),
+('123456789', '0002'),
+('987654321', '0001');
+('987654321', '0002')
+
+insert into Skill values
+('00000001', 'CPP'),
+('00000002', 'Java'),
+('00000003', 'Python'),
+('00000004', 'Animations'),
+('00000005', 'Two Pointer'),
+('00000006', 'Hashmapping'),
+('00000007', 'Algorithms'),
+('00000008', 'Scripting'),
+('00000009', 'Javascript'),
+('00000010', 'SDL Library');
+
+insert into Student_Skill values
+('111111111', '000000001', 17),
+('111111111', '000000010', 9),
+('111111111', '000000006', 11),
+('111111111', '000000007', 4),
+('000000000', '000000003', 2),
+('000000000', '000000008', 2),
+('999999999', '000000009', 99),
+('999999999', '000000008', 41),
+('999999999', '000000007', 13),
+('999999999', '000000004', 16),
+('123456789', '000000002', 20),
+('123456789', '000000005', 10),
+('123456789', '000000007', 15),
+('987654321', '000000001', 50),
+('987654321', '000000007', 50),
+('987654321', '000000006', 50),
+('987654321', '000000005', 50),
+('987654321', '000000010', 50);
+
+insert into Career values
+('0A0A0A0A0A', 'DevOps', 'Some description'),
+('AAAAA11111', 'Software Engineering', 'You engineer software'),
+('BA10E00025', 'Computer Security Systems');
+
+insert into Major_Career values
+('0000', '0A0A0A0A0A', 10.0),
+('0000', 'AAAAA11111', 10.0),
+('0000', 'BA10E00025', 10.0),
+('0001', 'BA10E00025', 50.0),
+('0001', 'AAAAA11111', 50.0),
+('0001', '0A0A0A0A0A', 50.0),
+('0002', '0A0A0A0A0A', 25.7),
+('0002', 'AAAAA11111', 13.94),
+('0002', 'BA10E00025', 71.1);
+
+insert into Career_Skill values
+('0A0A0A0A0A', '00000001', 15.4),
+('0A0A0A0A0A', '00000002', 66.7),
+('0A0A0A0A0A', '00000003', 66.7),
+('AAAAA11111', '00000001', 50),
+('AAAAA11111', '00000002', 50),
+('AAAAA11111', '00000003', 50),
+('AAAAA11111', '00000009', 60.5),
+('AAAAA11111', '00000007', 43.2),
+('BA10E00025', '00000001', 75),
+('BA10E00025', '00000002', 65),
+('BA10E00025', '00000003', 21.7),
+('BA10E00025', '00000008', 89),
+('BA10E00025', '00000007', 78);
+
+insert into Quest values
+('SWROKK2451WKF09', 'Learning CPP Basic Syntax', 'To learn the basic syntax of C++'),
+('21398SANWU83NNW', 'Learning Python Basic Syntax', 'To learn the basic syntax of Python'),
+('74917hdasn219sW', 'Basic Algorithms', 'To learn at least 3 basic algorithms, insert later idk'),
+('QWERTY123456789', 'Advanced Data Structures', 'Learn and implement advanced data structures'),
+('ASDFGH987654321', 'Web Development Basics', 'Create a simple website using HTML, CSS, and JavaScript');
+
+insert into Student_Quest values
+('111111111', 'SWROKK2451WKF09', 'In Progress'),
+('000000000', '21398SANWU83NNW', 'Completed'),
+('999999999', '74917hdasn219sW', 'Not Started'),
+('123456789', 'QWERTY123456789', 'In Progress'),
+('987654321', 'ASDFGH987654321', 'In Progress');
+
+INSERT INTO Resources VALUES
+('RES001', 'C++ Tutorial', 'https://www.cplusplus.com/doc/tutorial/', 'Comprehensive C++ programming tutorial'),
+('RES002', 'Python Official Documentation', 'https://docs.python.org/3/', 'Official Python programming language documentation'),
+('RES003', 'Introduction to Algorithms', 'https://mitpress.mit.edu/books/introduction-algorithms', 'Classic textbook on computer algorithms');
+
+INSERT INTO Quest_Resources VALUES
+('SWROKK2451WKF09', 'RES001'),
+('21398SANWU83NNW', 'RES002'),
+('74917hdasn219sW', 'RES003'),
+('QWERTY123456789', 'RES003'),
+('ASDFGH987654321', 'RES002');
