b7933380c90715663d218a06b8269f661c492ded
cjt101 <115830304+cjt101@users.noreply.github.com>
2024-10-15 12:39:30 -0700

    Small change regarding try catch

--- edutale/src/Mainpage/SkillGraph/SkillGraph.jsx
+++ edutale/src/Mainpage/SkillGraph/SkillGraph.jsx
@@ -1,11 +1,24 @@
 import * as SKILLS from "../../SKILLS.json"
+import Axios from "axios"
 import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement } from "chart.js"
 import { Radar } from "react-chartjs-2"
 
 ChartJS.register(RadialLinearScale, PointElement, LineElement)
 
 export default function SkillGraph() {
+    console.log(fetchStudentSkills())
     return (
       <Radar data={SKILLS} />
     )
+}
+const studentId = '111111111';
+
+async function fetchStudentSkills(){
+  try{
+    const response = await Axios.get(`http://localhost:3000/api/students/${studentId}/skills`);
+    const skills = response.data;
+    return skills;
+  } catch(err){
+    console.error('Error fetching skills: ', err);
+  }
 }
