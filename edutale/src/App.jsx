4817df640355e7f2e78bba8fc0725a9991732142
abigail-jn <116382538+abigail-jn@users.noreply.github.com>
2024-10-16 19:32:51 +0000

    Fixed most mainpage CSS involving resizing windows

--- edutale/src/App.jsx
+++ edutale/src/App.jsx
@@ -17,23 +17,27 @@ export default function App() {
   return (
     <>
       <div id="modal-root"></div>
-      <Link to="/settings" className="settings"><button> Settings </button></Link>
-      <Link to="/admin" className="admin"><button> Admin </button></Link>
-      
-      <div className="navigation">
-        <Navigation />
-        <div>
-          <Routes>
-            <Route path="/" className="disabled-link" element={<Mainpage />} />
-            <Route path="/check-in" className="disabled-link" element={<CheckIn />} />
-            <Route path="/inventory" className="disabled-link" element={<Inventory />} />
-            <Route path="/skill-list" className="disabled-link" element={<SkillList />} />
-            <Route path="/store" className="disabled-link" element={<Store />} />
-            <Route path="/settings" className="disabled-link" element={<Settings />} />
-            <Route path="/admin" className="disabled-link" element={<Admin />} />
-          </Routes>
+      <div className="top">
+        <div className="topPane">
+          <Link to="/settings" className="settings"><button> Settings </button></Link>
+          <Link to="/admin" className="admin"><button> Admin </button></Link>
+        </div>
+        <div className="topPane">
+          <Navigation />
         </div>
       </div>
+      <div>
+        <Routes>
+          <Route path="/" className="disabled-link" element={<Mainpage />} />
+          <Route path="/check-in" className="disabled-link" element={<CheckIn />} />
+          <Route path="/inventory" className="disabled-link" element={<Inventory />} />
+          <Route path="/skill-list" className="disabled-link" element={<SkillList />} />
+          <Route path="/store" className="disabled-link" element={<Store />} />
+          <Route path="/settings" className="disabled-link" element={<Settings />} />
+          <Route path="/admin" className="disabled-link" element={<Admin />} />
+        </Routes>
+      </div>
+      
     </>
   );
 }
