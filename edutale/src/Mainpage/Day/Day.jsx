ae28b722308d8337d49eb446b5d1db903f0924bb
cjt101 <115830304+cjt101@users.noreply.github.com>
2024-10-16 13:03:23 -0700

    Added functionality of backend to Day counter

--- edutale/src/Mainpage/Day/Day.jsx
+++ edutale/src/Mainpage/Day/Day.jsx
@@ -1,18 +1,38 @@
 import * as USER from "../../USER.json"
+import { useState, useEffect } from "react"
+import Axios from "axios"
+
+const studentId = "111111111"
 
 export default function Day() {
+    const [joinDate, setJoinDate] = useState()
+
+    useEffect(() => {
+        fetchStudentJoinDate()
+    }, [])
+
+    function dayNum(jd) {
+      let now = Date.now()
+      console.log(jd[0])
+      let joinDateParsed = Date.parse(jd)
+      return Math.round((now - joinDateParsed) / 86400000)
+    }
+
+    async function fetchStudentJoinDate() {
+      try {
+        await Axios.get(`http://localhost:3000/api/students/${studentId}`)
+            .then((response) => {
+              setJoinDate(dayNum(response.data[0].student_join_date))
+            })
+      }
+      catch(err) {
+        console.error('Error fetching skills: ', err);
+      }
+    }
+
     return (
       <div className="day">
-        <h1><u> Day {dayNum(USER.joinDate)} </u></h1>
+        <h1><u> Day {joinDate} </u></h1>
       </div>
     )
-}
-
-// calculates day number since the join date of the user.
-// done by subtracting the number of milliseconds between
-// now and the join date, then dividing by the num of ms/day.
-function dayNum(joinDate) {
-    let now = Date.now()
-    let joinDateParsed = Date.parse(joinDate)
-    return Math.round((now - joinDateParsed) / 86400000)
 }
