7295da5f692c07004e4d14103a4a0dccd7a94fee
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-10-20 22:18:18 -0700

    Change query for SkillList and add more dummy data

--- Backend/src/config/scripts/career/get_career_quest_list.sql
+++ Backend/src/config/scripts/career/get_career_quest_list.sql
@@ -1,7 +1,12 @@
--- gets the list of quests for a given career, and their skill.
-select q.quest_id, s.skill_name, q.quest_name, q.quest_description
-from Quest q, Skill s, Skill_Quest sq, Career_Skill cs
+-- given a career, returns all skills and all quests for each skill.
+-- also returns all resources associated with each skill. For use
+-- in the Skill List page.
+select q.quest_id, s.skill_name, q.quest_name, q.quest_description,
+       r.resource_id, r.resource_name, r.resource_link, r.resource_description
+from Quest q, Skill s, Skill_Quest sq, Career_Skill cs, Resources r, Quest_Resources qr
 where q.quest_id = sq.quest_id
       and sq.skill_id = s.skill_id
       and sq.skill_id = cs.skill_id
+      and q.quest_id = qr.quest_id
+      and r.resource_id = qr.resource_id
       and cs.career_id = $1;
