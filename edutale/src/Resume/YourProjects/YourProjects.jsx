e4abff1c02d73eee1ecc3ff9d60b895ff7e51149
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-10-22 23:03:35 -0700

    Refactor Resume code related to Axios calls

--- edutale/src/Resume/YourProjects/YourProjects.jsx
+++ edutale/src/Resume/YourProjects/YourProjects.jsx
@@ -1,45 +1,19 @@
-import React, { useState, useEffect } from "react"
-import Axios from "axios"
-
-const studentId = "TESTSTU01"
-
-export default function YourProjects() {
-    const [projects, setProjects] = useState()
-
-    useEffect(() => {
-        fetchStudentProjects()
-    }, [])
-
-    async function fetchStudentProjects() {
-        try {
-            await Axios.get(`http://localhost:3000/api/students/${studentId}/quests`)
-                .then((response) => {
-
-                    // sort by highest to lowest difficulty (Expert -> Inter. -> Beg.)
-                    response.data.sort(function(a,b) {
-                        return a.quest_difficulty - b.quest_difficulty
-                    }).reverse()
-
-                    // set skills to the formatted HTML for the component
-                    setProjects(
-                      <>
-                        {response.data.map(item => item.is_project && (
-                          <p key={item.quest_id}> {item.quest_name}: {item.quest_description} </p>
-                        ))}
-                      </>
-                    )
-                })
-        }
-        catch(err) {
-            console.error("Error fetching projects: ", err)
-        }
+export default function YourProjects({projects}) {
+    function sortProjects(projects) {
+        return (
+            projects.sort(function(a,b) {
+                return a.quest_difficulty - b.quest_difficulty
+            }).reverse()
+        )
     }
 
-    return (
+    return projects && (
       <>
         <div>
           <h1 className="header-centered"> <u> Your Projects </u> </h1>
-          {projects}
+          {sortProjects(projects).map(item => item.is_project && (
+            <p key={item.quest_id}> {item.quest_name}: {item.quest_description} </p>
+          ))}
         </div>
       </>
     )
