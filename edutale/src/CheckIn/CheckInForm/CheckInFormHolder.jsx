a48fb890581a0968f445ae44579c062615319f21
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-10-25 23:06:14 -0700

    Add functionality for pulling ongoing quests for the Check In form

---
+++ edutale/src/CheckIn/CheckInForm/CheckInFormHolder.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react"
+import Axios from "axios"
+import CheckInFormContent from "./CheckInFormContent"
+
+const studentId = "TESTSTU01"
+
+export default function CheckInFormHolder() {
+    const [quests, setQuests] = useState()
+
+    useEffect(() => {
+        fetchStudentQuests()
+    }, [])
+
+    async function fetchStudentQuests() {
+        try {
+            await Axios.get(`http://localhost:3000/api/students/${studentId}/quests`)
+                .then((response) => {
+                    let namesAndIds = []
+                    response.data.map((item) => {
+                        namesAndIds.push({
+                            quest_name: item.quest_name,
+                            quest_id: item.quest_id
+                        })
+                    })
+
+                    setQuests(namesAndIds)
+                })
+        }
+        catch(err) {
+            console.error("Error fetching ongoing quests: ", err)
+        }
+    }
+
+    return (
+        <CheckInFormContent quests={quests} />
+    )
+}
