b1543152209aba99dee47bfc75b0fc591a983005
LingCPU <115831204+LingCPU@users.noreply.github.com>
2024-10-26 21:57:44 -0700

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,8 +5,8 @@ const testRoutes = require('./routes/test') // remove later
 const studentRoutes = require('./routes/students')
 const questRoutes = require('./routes/quests')
 const careerRoutes = require('./routes/careers')
-const skillRoutes = require('./routes/skills')
-const resourceRoutes = require('./routes/resources')
+const inventoryRoutes = require('./routes/inventory')
+//const skillRoutes = require('./routes/skills')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,11 +17,16 @@ app.use('/api', testRoutes) // remove later
 app.use('/api/students', studentRoutes)
 app.use('/api/quests', questRoutes)
 app.use('/api/careers', careerRoutes)
-app.use('/api/skills', skillRoutes)
-app.use('/api/resources', resourceRoutes)
+app.use('/api/inventory', inventoryRoutes)
+//app.use('/api/skills', skillRoutes)
 
 app.get('/', (req, res)=>{
     res.send('Edutale Backend is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
 })
 
 app.listen(port, ()=>{
