05897d85b2e63d52f93851cb22f56aef9e9f80c1
LingCPU <tommyle2489@gmail.com>
2024-10-26 22:03:20 -0700

    fix server.js 2.0

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -27,7 +27,3 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
