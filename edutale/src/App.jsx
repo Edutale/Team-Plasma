9a8943630e2cdf9e17ed66f6c9babccf1bb4f3de
abigail-jn <ajp165@humboldt.edu>
2024-10-29 01:53:12 -0700

    Establish protected routes

--- edutale/src/App.jsx
+++ edutale/src/App.jsx
@@ -1,6 +1,7 @@
 /*--- App.jsx ---*/ 
 
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route, Navigate, Outlet } from "react-router-dom"
+import { useAuth0 } from "@auth0/auth0-react"
 
 import Welcome from "./Welcome/Welcome"
 import Mainpage from "./Mainpage/Mainpage"
@@ -14,18 +15,31 @@ import Admin from "./Admin/Admin"
 import "./App.css"
 
 // Note: <Routes> element renders whatever element is contained in the selected Route.
+
+const ProtectedRoute = ({ redirectTo }) => {
+  const { isAuthenticated, isLoading  } = useAuth0()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
+}
+
 export default function App() {
     return (
     <>
       <Routes>
         <Route path="/welcome" className="disabled-link" element={<Welcome />} />
-        <Route path="/" className="disabled-link" element={<Mainpage />} />
-        <Route path="/check-in" className="disabled-link" element={<CheckIn />} />
-        <Route path="/inventory" className="disabled-link" element={<Inventory />} />
-        <Route path="/skill-list" className="disabled-link" element={<SkillGallery />} />
-        <Route path="/resume" className="disabled-link" element={<Resume />} />
-        <Route path="/settings" className="disabled-link" element={<Settings />} />
-        <Route path="/admin" className="disabled-link" element={<Admin />} />
+        <Route element={<ProtectedRoute redirectTo="/welcome" />}>
+          <Route path="/" className="disabled-link" element={<Mainpage />} />
+          <Route path="/check-in" className="disabled-link" element={<CheckIn />} />
+          <Route path="/inventory" className="disabled-link" element={<Inventory />} />
+          <Route path="/skill-list" className="disabled-link" element={<SkillGallery />} />
+          <Route path="/resume" className="disabled-link" element={<Resume />} />
+          <Route path="/settings" className="disabled-link" element={<Settings />} />
+          <Route path="/admin" className="disabled-link" element={<Admin />} />
+        </Route>      
       </Routes>
     </>
     )
