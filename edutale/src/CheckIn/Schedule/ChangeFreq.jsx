e4c8217c0e1004588c52a0f623d9bf577333debb
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-11-05 23:49:15 -0800

    Bare implementation to understand how to create .ics files

--- edutale/src/CheckIn/Schedule/ChangeFreq.jsx
+++ edutale/src/CheckIn/Schedule/ChangeFreq.jsx
@@ -1,5 +1,7 @@
 // creates modal for the "change frequency" button.
 import Popup from "reactjs-popup"
+import { createEvent } from "ics"
+import { saveAs } from "file-saver"
 
 export default function ChangeFreq() {
     return (
@@ -31,9 +33,37 @@ export default function ChangeFreq() {
 function FreqModal() {
     return (
       <div className="freq-modal">
-        <button> Daily </button>
+        <button onClick={createCal}> Daily </button>
         <button> Weekly </button>
         <button> Monthly </button>
       </div>
     )
   }
+
+function createCal() {
+    let startDate = new Date()
+    startDate.setDate(startDate.getDate() + (6 + 7 - startDate.getDay()) % 7);
+    startDate.setHours(8,0,0,0)
+    //startDate.toISOString()
+
+    let event = {
+        start: startDate.getTime(),
+        startInputType: "utc",
+        duration: { hours: 1 },
+        title: "Edutale Reminder",
+        description: "It's time for your check-in for Edutale!",
+        recurrenceRule: "FREQ=WEEKLY;BYDAY=SA;INTERVAL=1",
+        }
+    
+    createEvent(event, (error, value) => {
+        if (error) {
+            console.log(error)
+        }
+
+        const blob = new Blob([value], { type: "text/calendar" });
+        saveAs(blob, `edutale-calendar.ics`);
+    })
+    
+
+
+}
