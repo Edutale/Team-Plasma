8440993dbcdc383baa366dfa76d90a2027086c90
Cheyenne Ty <115830304+cjt101@users.noreply.github.com>
2024-11-06 21:58:34 -0800

    Add other two options for the calendar schedule

--- edutale/src/CheckIn/Schedule/ChangeFreq.jsx
+++ edutale/src/CheckIn/Schedule/ChangeFreq.jsx
@@ -33,26 +33,51 @@ export default function ChangeFreq() {
 function FreqModal() {
     return (
       <div className="freq-modal">
-        <button onClick={createCal}> Daily </button>
+        <button onClick={() => createCal("M")}> Daily </button>
         <button> Weekly </button>
         <button> Monthly </button>
       </div>
     )
   }
 
-function createCal() {
+function createCal(frequency) {
+    // today's date
     let startDate = new Date()
-    startDate.setDate(startDate.getDate() + (6 + 7 - startDate.getDay()) % 7);
+    let recurrence = ""
+
+    // obtain recurrence rule and start date dependent on frequency given
+    switch(frequency) {
+        case "D":
+            recurrence = "FREQ=DAILY;INTERVAL=1"
+            break
+        case "W":
+            // changes startDate's date to the next Sun
+            startDate.setDate(startDate.getDate() + (7 + 7 - startDate.getDay()) % 7)
+            recurrence = "FREQ=WEEKLY;BYDAY=SU;INTERVAL=1"
+            break
+        case "M":
+            // changes startDate's date to the first of the next month
+            startDate.setMonth(startDate.getMonth() + 1, 1)
+            recurrence = "FREQ=MONTHLY;INTERVAL=1;BYMONTHDAY=1"
+            break
+    }
+
+    // changes startDate's date to the next Saturday (or today if today's Saturday)
+    startDate.setDate(startDate.getDate() + (7 + 7 - startDate.getDay()) % 7)
+    
+    // sets hour to 8AM as per the business rules
     startDate.setHours(8,0,0,0)
-    //startDate.toISOString()
 
     let event = {
         start: startDate.getTime(),
         startInputType: "utc",
         duration: { hours: 1 },
-        title: "Edutale Reminder",
+        title: "Edutale Check-in",
+        calName: "Edutale Reminder Calendar",
         description: "It's time for your check-in for Edutale!",
-        recurrenceRule: "FREQ=WEEKLY;BYDAY=SA;INTERVAL=1",
+        location: "http://localhost:5173/check-in", 
+        url: "http://localhost:5173/check-in",
+        recurrenceRule: recurrence,
         }
     
     createEvent(event, (error, value) => {
@@ -60,6 +85,7 @@ function createCal() {
             console.log(error)
         }
 
+        // the Blob will be the downloaded .ics file the user can add to their calendar app
         const blob = new Blob([value], { type: "text/calendar" });
         saveAs(blob, `edutale-calendar.ics`);
     })
