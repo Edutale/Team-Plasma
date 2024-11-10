902cb3d66436216ab0412fb5b63887037a379822
abigail-jn <ajp165@humboldt.edu>
2024-11-09 21:23:20 -0800

    fix quest board so it only pulls quests the student hasnt started or completed

--- edutale/src/Mainpage/QuestBoard/QuestBoard.jsx
+++ edutale/src/Mainpage/QuestBoard/QuestBoard.jsx
@@ -18,64 +18,88 @@ let QUESTS = myJson["quests"]
  * to reflect both functionalities.  Changing to pull from database in future.*/
 export default function QuestBoard() {
     const [quests, setQuests] = useState()
+    const [studentQuests, setStudentQuests] = useState()
+
     useEffect(() => {
         fetchQuests()
+    }, [])
+
+    useEffect(() => {
+        fetchStudentQuests()
     }, [])
 
     async function fetchQuests() {
         try {
           await Axios.get(`http://localhost:3000/api/quests`)
-            .then((response) => {
-                let questData = []
-                for (const {quest_id, quest_name, quest_description} of response.data.slice(0, 5)) {
-                    questData.push({
-                        id: quest_id,
-                        name: quest_name,
-                        desc: quest_description
-                    })
-                }
+          .then((response) => {
+              let questData = []
+              for (const {quest_id, quest_name, quest_description} of response.data) {
+                  questData.push({
+                      id: quest_id,
+                      name: quest_name,
+                      desc: quest_description
+                  })
+              }
+              setQuests(questData)
+        })
+        }
+        catch(err) {
+            console.error("Error fetching quests: ", err)
+        }
+    }
 
-                setQuests(
-                  <>
-                  {questData.map(item => (
-                    <Popup key={item.name} trigger= {
-                      <button className="row-button">
-                        <QuestRow img={QUESTS[0].img} qName={item.name} desc={item.desc} />
-                      </button>}
-                      modal nested>{
-                        close => (
-                          <div className="quest-modal">
-                            <div className="modal-header">
-                              <button className="modal-header-button close" onClick={() => close()}>
-                                ⨯
-                              </button>
-                            </div>
-                            <QuestModal qName={item.name} qDesc={item.desc} />
-                            <div className="quest-footer">
-                              <button className="modal-footer-button accept" onClick={() => AcceptHandler(studentId, item.id)}>
-                                Accept Quest
-                              </button>
-                            </div>
-                          </div>
-                        )
-                      }                           
-                    </Popup>
-                  ))}
-                  </>
-                )  
+    async function fetchStudentQuests() {
+        try {
+            await Axios.get(`http://localhost:3000/api/students/${studentId}/quests`)
+            .then((response) => {
+              setStudentQuests(response.data.map((item) => item.quest_id))
             })
         }
         catch(err) {
             console.error("Error fetching quests: ", err)
-        }  
-    }
+        }
+  }
 
-    return (
+    return quests && studentQuests && (
       <div className="questBoard">
         <h1><u> Quest Board </u></h1>
         <div className="qBoard-container">
-          {quests}
+          {makeBoard(quests, studentQuests)}
         </div>
       </div>
     )
-};
+}
+
+
+function makeBoard(qsts, stuQsts) {
+    let boardQuests = qsts.filter((item) => { if (!stuQsts.includes(item.id)) return item})
+
+    return (
+      <>
+      {boardQuests.slice(0,5).map(item => (
+        <Popup key={item.name} trigger= {
+          <button className="row-button">
+            <QuestRow img={QUESTS[0].img} qName={item.name} desc={item.desc} />
+          </button>}
+          modal nested>{
+            close => (
+              <div className="quest-modal">
+                <div className="modal-header">
+                  <button className="modal-header-button close" onClick={() => close()}>
+                    ⨯
+                  </button>
+                </div>
+                <QuestModal qName={item.name} qDesc={item.desc} />
+                <div className="quest-footer">
+                  <button className="modal-footer-button accept" onClick={() => AcceptHandler(studentId, item.id)}>
+                    Accept Quest
+                  </button>
+                </div>
+              </div>
+            )
+          }                           
+        </Popup>
+      ))}
+      </>
+    )  
+}
