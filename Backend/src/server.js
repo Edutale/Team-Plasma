2c23986067239507a30ccc6c40dcfd9da197fa8f
cjt101 <115830304+cjt101@users.noreply.github.com>
2024-12-11 12:16:40 -0800

    Final changes and removed testing files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 require("dotenv").config({path: "../.env"})
 const cors = require("cors")
-const testRoutes = require("./routes/test") // remove later
 const studentRoutes = require("./routes/students")
 const questRoutes = require("./routes/quests")
 const careerRoutes = require("./routes/careers")
@@ -14,7 +13,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
-app.use("/api", testRoutes) // remove later
 app.use("/api/students", studentRoutes)
 app.use("/api/quests", questRoutes)
 app.use("/api/careers", careerRoutes)
